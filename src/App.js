7e3adb87470a1abc273ba3a1dec0b7c64687c5e3
Xuan Hung <hungdx@coin98.finance>
2024-07-15 15:16:30 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import './App.css';
 
 function App() {
 
+  const openPopupWindow = (url, { title, ...customOptions }) => {
+    return window.open(url, title, optionToString({ ...defaultOptions, ...customOptions }))
+  }
+
   const handleSocialLogin = () => {
-    const opened = setTimeout(() => {
-      window.open('https://auth.v2.ramper.xyz/wallet/signin')
-      if (!opened) alert('please allow popup setting')
-    }, 1000);
+    openPopupWindow(
+      `https://auth.v2.ramper.xyz/transaction/send-token?${new URLSearchParams({
+        from: '0x835d5e132039987A994c6288777DbF7d1Bb511A5',
+        to: '0x835d5e132039987A994c6288777DbF7d1Bb511A5',
+        symbol: 'VIC',
+        //@ts-ignore
+        denom: '',
+        //@ts-ignore
+        coingeckoId: '0',
+        decimal: String(0),
+        amount: 0
+      })}`,
+      {
+        title: `App Request-${String(Math.random()).slice(-2)}`,
+        height: 650,
+        width: 400
+      }
+    )
   }
 
   return (
